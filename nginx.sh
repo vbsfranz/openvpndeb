ae4b0c5041f01a03b76e8a28b1673c0abfaa4e10
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-13 15:10:07 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -3,4 +3,5 @@ mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 cp ~/openvpndeb/nginx.conf /etc/nginx/nginx.conf
 rm /etc/nginx/conf.d/*.conf
 cp ~/openvpndeb/ocs.conf /etc/nginx/conf.d/
+service nginx restart
 rm -Rf ~/openvpndeb/

f6c68f4f8ae970f766c9ea2b0463a76555977d6c
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-13 12:26:45 +0800

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+cp ~/openvpndeb/nginx.conf /etc/nginx/nginx.conf
+rm /etc/nginx/conf.d/*.conf
+cp ~/openvpndeb/ocs.conf /etc/nginx/conf.d/

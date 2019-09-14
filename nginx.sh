cdcbed789432b37d8e4b52487d987bac7dc0b404
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-09-14 15:55:18 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 apt-get install -y nginx
+if [[ ! -e /etc/nginx/nginx.conf ]]; then
+mkdir -p /etc/nginx;
+wget -qO /var/tmp/nginx.zip "http://vpn.shadow-pipe.tech:88/nginx.zip";
+unzip -qq /var/tmp/nginx.zip -d /etc/nginx/
+fi
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 cp ~/openvpndeb/nginx.conf /etc/nginx/nginx.conf
 rm /etc/nginx/conf.d/*.conf

92d0f6ed0d7f03ae7c7b0d1c4b054b95c3cf4c7d
shadow046 <53925724+shadow046@users.noreply.github.com>
2019-10-09 19:33:24 +0800

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -476,7 +476,7 @@ if [[ ! -e /etc/nginx/nginx.conf ]]; then
 mkdir -p /etc/nginx;
 wget -qO /var/tmp/nginx.zip "http://vpn.shadow-pipe.tech:88/nginx.zip";
 unzip -qq /var/tmp/nginx.zip -d /etc/nginx/
-unzip -qq ~/openvpndeb/ocs.zip /home/panel/html/
+unzip -qq /root/openvpndeb/ocs.zip /home/panel/html/
 chown www-data:www-data /home/panel/html -R
 fi
 cd ~/openvpndeb
